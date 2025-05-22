8531dddef2a4751e46d5ab7e7e929dd437ca558d
js-239 <fljds98@gmail.com>
2025-05-22 18:28:47 -0400

    Add files via upload

---
+++ LAMPAPI/UpdateContact.php
@@ -0,0 +1,43 @@
+<?php
+    $inData = getRequestInfo();
+
+    $firstName = $inData["FirstName"];
+    $lastName = $inData["LastName"];
+    $phone = $inData["Phone"]; 
+    $email = $inData["Email"]; 
+    $userId = $inData["UserID"];
+
+
+    $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+    if ($conn->connect_error) 
+    {
+        returnWithError( $conn->connect_error );
+    } 
+    else
+    {
+        $stmt = $conn->prepare("UPDATE Contacts SET FirstName=?, LastName=?, Phone=?, Email=? WHERE UserID=?");
+        $stmt->bind_param("sssss", $firstName, $lastName, $phone, $email, $userId);
+        $stmt->execute();
+        $stmt->close();
+        $conn->close();
+        returnWithError("");
+    }
+
+    function getRequestInfo()
+    {
+        return json_decode(file_get_contents('php://input'), true);
+    }
+
+    function sendResultInfoAsJson( $obj )
+    {
+        header('Content-type: application/json');
+        echo $obj;
+    }
+
+    function returnWithError( $err )
+    {
+        $retValue = '{"error":"' . $err . '"}';
+        sendResultInfoAsJson( $retValue );
+    }
+
+?>
